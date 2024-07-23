ee52ab79f7c9b26fb4d17add6cc7fbaf0537bc65
srknr <k_srkn13@yahoo.co.jp>
2024-07-23 18:38:02 +0900

    Updates

---
+++ precache-manifest.d56ba7740f332abf7a7ec4e274c6bfbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2790247589235bbb33edd1b225a84a",
+    "url": "/create_bill_of_purchase/index.html"
+  },
+  {
+    "revision": "fcc97310d86f69c82ced",
+    "url": "/create_bill_of_purchase/static/css/main.07dd9d39.chunk.css"
+  },
+  {
+    "revision": "94e513a5da43fffeacf6",
+    "url": "/create_bill_of_purchase/static/js/2.ad3b07eb.chunk.js"
+  },
+  {
+    "revision": "fb83666f81dac017fe55e5b9c1fb53d1",
+    "url": "/create_bill_of_purchase/static/js/2.ad3b07eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480751b1ecf630da032f",
+    "url": "/create_bill_of_purchase/static/js/3.82129812.chunk.js"
+  },
+  {
+    "revision": "fcc97310d86f69c82ced",
+    "url": "/create_bill_of_purchase/static/js/main.5ad90868.chunk.js"
+  },
+  {
+    "revision": "b0580dad542856447970",
+    "url": "/create_bill_of_purchase/static/js/runtime-main.4e7cdc20.js"
+  }
+]);
